34cd4650eeaf00bbb853c4ea2076c74bc566ed2a
Fatima Pereira <pfati.34@gmail.com>
2016-11-15 16:33:43 +0100

    ADD buttonPicassoPhoto
    Inserer newActivity à un bouton et insérer une image avec Library
    Picasso

--- app/src/main/java/com/pereira/fatima/holdall/PicassoActivity.java
+++ app/src/main/java/com/pereira/fatima/holdall/PicassoActivity.java
@@ -2,6 +2,9 @@ package com.pereira.fatima.holdall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class PicassoActivity extends AppCompatActivity {
 
@@ -9,6 +12,13 @@ public class PicassoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picasso);
+
+        String url ="http://humourtop.com/images-insolites-droles-5/Avion_Afrique_insolite.jpg";
+        ImageView imageView = (ImageView) findViewById(R.id.image_picasso);
+
+        Picasso.with(this).load(url).into(imageView);
+
+
 
     }
 }
