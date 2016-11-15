a1d62924113fe522ce2421e51d7689757a5cdb3d
Fatima Pereira <pfati.34@gmail.com>
2016-11-15 14:41:35 +0100

    ADD toast
    add button + toast

--- app/src/main/java/com/pereira/fatima/holdall/MainActivity.java
+++ app/src/main/java/com/pereira/fatima/holdall/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
 //        TextView textView = (TextView) findViewById(R.id.text);
 //        textView.setText("Bonjour");
 
-        Button button = (Button) findViewById(R.id.test);
+        Button button = (Button) findViewById(R.id.test1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this, "ça marche", Toast.LENGTH_LONG).show();
             }
         });
 
