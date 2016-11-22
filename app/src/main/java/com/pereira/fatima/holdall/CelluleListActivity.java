4cc6b2edd0d276c4bace039d9c292da402703c80
Fatima Pereira <pfati.34@gmail.com>
2016-11-22 13:37:40 +0100

    UPDATE CelluleList
    Création de tous les fichiers xml, activity et class

--- app/src/main/java/com/pereira/fatima/holdall/CelluleListActivity.java
+++ app/src/main/java/com/pereira/fatima/holdall/CelluleListActivity.java
@@ -9,6 +9,6 @@ public class CelluleListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cellule_list);
-        
+
     }
 }
---
+++ app/src/main/java/com/pereira/fatima/holdall/CelluleListAdapter.java
@@ -0,0 +1,10 @@
+package com.pereira.fatima.holdall;
+
+/**
+ * Created by Human Booster on 22/11/2016.
+ */
+
+public class CelluleListAdapter {
+
+
+}
