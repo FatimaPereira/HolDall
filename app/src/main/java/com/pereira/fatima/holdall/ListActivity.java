a0928cf2e2e44ef06e6c32dfa1c6249282121a20
Fatima Pereira <pfati.34@gmail.com>
2016-11-22 10:44:47 +0100

    UPDATE listActivity
    Création liste

--- app/src/main/java/com/pereira/fatima/holdall/ListActivity.java
+++ app/src/main/java/com/pereira/fatima/holdall/ListActivity.java
@@ -3,12 +3,19 @@ package com.pereira.fatima.holdall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ListActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        ArrayList<String>listEssai = new ArrayList<>();
+        listEssai.add("essai 1");
+        listEssai.add("essai 2");
+        listEssai.add("essai 3");
     }
 }
