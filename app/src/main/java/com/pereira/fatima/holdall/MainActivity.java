d40b2a5f94f3d9e61cfbe7a855be7a2d9e1eb0b9
Fatima Pereira <pfati.34@gmail.com>
2016-11-22 10:27:05 +0100

    UPDATE btnListActivity
    Lier le bouton à une new view

--- app/src/main/java/com/pereira/fatima/holdall/MainActivity.java
+++ app/src/main/java/com/pereira/fatima/holdall/MainActivity.java
@@ -53,6 +53,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button buttonList = (Button) findViewById(R.id.test4);
+        buttonList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Envoyer vers une Activity = Intent
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
 //        Button btnColorPalindrone = (Button) findViewById(R.id.fab);
 //        buttonPalindrone.setOnClickListener(new View.OnClickListener() {
 //            @Override
